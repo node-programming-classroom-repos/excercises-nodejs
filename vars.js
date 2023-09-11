ed9d885a78efb56804c0cc8fa5ce1cb5c527aa34
Mattias Schertell <schertell@gmail.com>
2023-09-11 11:12:25 +0200

    example added

---
+++ vars.js
@@ -0,0 +1,32 @@
+// Deklarera en variabel och tilldela ett heltal
+var age = 25;
+console.log("Ålder:", age);
+
+// Deklarera en variabel och tilldela en textsträng
+var name = "Alice";
+console.log("Namn:", name);
+
+// Deklarera en variabel och tilldela ett flyttal
+var temperature = 28.5;
+console.log("Temperatur:", temperature);
+
+// Deklarera en variabel och tilldela en boolesk sanning
+var isActive = true;
+console.log("Aktiv?", isActive);
+
+// Deklarera en array och fyll den med olika datatyper
+var mixedArray = ["Hej", 42, true, 3.14];
+console.log("Blandad array:", mixedArray);
+
+// Utföra operationer med variabler
+var num1 = 10;
+var num2 = 5;
+var sum = num1 + num2;
+console.log("Summa:", sum);
+
+var greeting = "Hej " + name;
+console.log(greeting);
+
+var isGreater = num1 > num2;
+console.log("Är num1 större än num2?", isGreater);
+
