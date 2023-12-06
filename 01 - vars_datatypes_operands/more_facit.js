0824f23b847f43ca997fffd9cc0317ef682e1968
Mattias Schertell <schertell@gmail.com>
2023-12-06 17:10:09 +0100

    Create more_facit.js

---
+++ 01 - vars_datatypes_operands/more_facit.js
@@ -0,0 +1,144 @@
+//OBS - du bör skriva ut alla dina resultat (t ex variabler etc) med console.log
+//nedan är förslag på lösningar till övningarna i more.md
+
+let tempInCelsius = 25;
+console.log(tempInCelsius);
+
+
+const yearsOfExperience = 3;
+
+
+
+let isWeekend = false;
+
+
+
+let initialBalance = 1000;
+let expenses = 300;
+let remainingBalance = initialBalance - expenses;
+
+
+
+let distanceInKilometers = 150;
+let timeInHours = 2.5;
+let averageSpeed = distanceInKilometers / timeInHours;
+
+
+
+const radius = 5;
+let circleCircumference = 2 * Math.PI * radius;
+
+
+
+const currentYear = 2023;
+const birthYear = 1990;
+let age = currentYear - birthYear;
+
+
+
+let priceBeforeTax = 80;
+const taxRate = 0.25;
+let totalPrice = priceBeforeTax * (1 + taxRate);
+
+
+
+const secondsInAMinute = 60;
+const minutesInAnHour = 60;
+let secondsInAnHour = secondsInAMinute * minutesInAnHour;
+
+
+
+let productPrice = 50;
+const discountPercentage = 15;
+let discountedPrice = productPrice * (1 - discountPercentage / 100);
+
+
+
+let sideLength = 10;
+let squareArea = sideLength * sideLength;
+
+
+
+let depositAmount = 1000;
+const interestRate = 0.05;
+let totalAmountAfterInterest = depositAmount * (1 + interestRate);
+
+
+
+const numberOfPages = 300;
+let pagesRead = 100;
+let pagesLeft = numberOfPages - pagesRead;
+
+
+
+let totalDistance = 400;
+let fuelEfficiency = 20;
+let remainingDistance = totalDistance / fuelEfficiency;
+
+
+
+let purchaseAmount = 200;
+const cashbackPercentage = 5;
+let cashbackAmount = purchaseAmount * (cashbackPercentage / 100);
+
+
+
+let originalPrice = 120;
+let discountAmount = 30;
+let discountedPrice = originalPrice - discountAmount;
+
+
+
+let baseSalary = 50000;
+const bonusPercentage = 10;
+let totalSalary = baseSalary * (1 + bonusPercentage / 100);
+
+
+
+const numberOfQuestions = 20;
+let correctAnswers = 16;
+let percentageCorrect = (correctAnswers / numberOfQuestions) * 100;
+
+
+
+let loanAmount = 10000;
+
+
+const interestRate = 0.08;
+let totalAmountAfterInterest = loanAmount * (1 + interestRate);
+
+
+
+let containerVolume = 1000;
+let liquidVolume = 300;
+let remainingVolume = containerVolume - liquidVolume;
+
+
+
+let currentSpeed = 80;
+const speedLimit = 60;
+let speedOverLimit = currentSpeed - speedLimit;
+
+
+
+let numberOfGuests = 10;
+let totalCost = 500;
+let costPerGuest = totalCost / numberOfGuests;
+
+
+
+let totalWeight = 1000;
+let itemWeight = 300;
+let remainingWeight = totalWeight - itemWeight;
+
+
+
+let initialValue = 500;
+const rateOfDepreciation = 0.1;
+let currentValue = initialValue * (1 - rateOfDepreciation);
+
+
+
+let currentBalance = 1000;
+let withdrawalAmount = 200;
+let remainingBalance = currentBalance - withdrawalAmount;
